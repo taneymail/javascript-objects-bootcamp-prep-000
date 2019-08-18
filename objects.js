d9ff3185b973e5348a4a92082b19ba41469ae9d5
TShipley <taneymail@gmail.com>
2019-08-18 06:18:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlist = new Object ({artistName : songTitle})
-}
+var playlist = new Object ({artistName : songTitle});
+
 
 var updatePlaylist (playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName] : songTitle})
