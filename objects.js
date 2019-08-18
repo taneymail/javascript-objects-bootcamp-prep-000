4d864018ea54762dd06514e35af33800cbc99f91
TShipley <taneymail@gmail.com>
2019-08-18 06:21:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object ({ artistName: 'songTitle' });
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle})
+  return Object.assign({}, playlist, { [Phil Ochs]: myBloodyValentine})
 }
 
 function removeFromPlaylist(playlist, artistName) {
